f1067b95fdf6d71fd7df167b98005c58eb62d569
fwtrodrigo <fwtrodrigo@gmail.com>
2021-01-24 02:19:28 -0300

    Adicao Splash Screen

--- app/src/main/java/com/example/dhdesafiofirebase/SplashActivity.kt
+++ app/src/main/java/com/example/dhdesafiofirebase/SplashActivity.kt
@@ -1,16 +1,31 @@
 package com.example.dhdesafiofirebase
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dhdesafiofirebase.databinding.ActivitySplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
+    private val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        scope.launch {
+            delay(3000)
+
+            startActivity(Intent(application, MainActivity::class.java))
+            finish()
+        }
+
     }
 }
